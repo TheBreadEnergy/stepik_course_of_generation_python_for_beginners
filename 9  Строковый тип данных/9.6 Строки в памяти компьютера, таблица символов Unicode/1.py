cdc1f2cf225861456554a391abd0b2cddb916dcc
TheBreadEnergy <50107547+TheBreadEnergy@users.noreply.github.com>
2022-01-12 07:19:48 +0300

    9.6

---
+++ 9  Строковый тип данных/9.6 Строки в памяти компьютера, таблица символов Unicode/1.py
@@ -0,0 +1,4 @@
+chr1, chr2 = int(input()), int(input())
+
+for i in range(chr1, chr2+1):
+    print(chr(i), end=' ')
---
+++ 9  Строковый тип данных/9.6 Строки в памяти компьютера, таблица символов Unicode/3.py
@@ -0,0 +1,11 @@
+n = int(input())
+s = input()
+for i in range(len(s)):
+    f = s[i]
+    a = ord(s[i]) + (26 - n)
+    if 97 <= a <= 122:
+        print(chr(a), end='')
+        continue
+    if a > 122:
+        print(chr(96 + (a - 122)), end='')
+        continue
