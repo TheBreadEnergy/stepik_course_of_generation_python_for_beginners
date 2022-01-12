41b6f16b2bf20ec34a502f7d8bb7152829be1ecc
TheBreadEnergy <50107547+TheBreadEnergy@users.noreply.github.com>
2022-01-12 08:16:29 +0300

    11.2

---
+++ 11  Списки/11.2 Основы работы со списками/6.py
@@ -0,0 +1,8 @@
+rainbow = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet']
+rainbow[3], rainbow[-1] = 'Зеленый', 'Фиолетовый'
+
+print(rainbow)
+
+
+
+
---
+++ 11  Списки/11.2 Основы работы со списками/7.py
@@ -0,0 +1,3 @@
+rainbow = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet']
+rainbow[rainbow.index('Green')], rainbow[rainbow.index('Violet')] = 'Зеленый', 'Фиолетовый'
+print(rainbow)
