a607cea98683a08170e06fcacdee955180f33956
TheBreadEnergy <50107547+TheBreadEnergy@users.noreply.github.com>
2022-04-11 20:26:32 +0300

    14.1

---
+++ 14 Итоговая работа на функции/14.1 Задачи на программирование/1.py
@@ -0,0 +1,8 @@
+# объявление функции
+def draw_triangle():
+    for i in range(1, 9):
+        print((8 - i) * ' ' + '*' * (i * 2 - 1))
+
+
+# основная программа
+draw_triangle()  # вызов функции
---
+++ 14 Итоговая работа на функции/14.1 Задачи на программирование/6.py
@@ -0,0 +1,11 @@
+# объявление функции
+def is_magic(date):
+    day, month, year = date.split('.')
+    return int(day) * int(month) == int(year) % 100
+
+
+# считываем данные
+date = input()
+
+# вызываем функцию
+print(is_magic(date))
